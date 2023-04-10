1757f8cdbd16b741c246aecd61e19306284f98d1
Troy The Technical Writer <troy@troythetechnicalwriter.online>
2023-04-10 11:46:34 -0500

    docker build things

--- source-code/run-docker-image-of-api.sh
+++ source-code/run-docker-image-of-api.sh
@@ -18,7 +18,11 @@ echo "*** "
 echo "*** Docker Container START: textformatdashboard.online/text-format-dashboard-api-java"
 echo "*** "
 
-docker run -p 44444:8080 textformatdashboard.online/text-format-dashboard-api-java
+# docker run -p 44444:8080 textformatdashboard.online/text-format-dashboard-api-java
+
+docker-compose down
+
+docker-compose up -d
 
 ## 
 
